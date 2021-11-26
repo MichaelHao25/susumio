5265a905cabeb3b121a8c06a3e448d8235cdc0cd
blue <michael.hao25@gmail.com>
2021-11-27 03:05:11 +0800

    首页滚动不正确的问题修复

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    amd: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/ban-ts-comment": "off",
+    "react/display-name": "off",
+    "react/react-in-jsx-scope": "off",
+    // "indent": [
+    //   "error",
+    //   2
+    // ],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+  },
+};
